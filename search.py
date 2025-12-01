9d8c13497d0b61eeae1942e2af72c37f51010d3d
Nathan Pham <nathanp3@uci.edu>
2025-12-01 01:18:46 -0800

    Implement partial index utilization in search

--- search.py
+++ search.py
@@ -2,25 +2,39 @@ from nltk.stem import PorterStemmer
 import json
 import math
 from collections import defaultdict
-from indexer import tokenize
+from indexer import CORPUS, tokenize
+import os
+import pickle
+from merged_index_splitter import SECONDARY_BODY_INDEX_PATH, SECONDARY_IMPORTANT_INDEX_PATH, SPLIT_OUTPUT_BODY_DIR as BODY_INDEX_DIR, SPLIT_OUTPUT_IMPORTANT_DIR as IMPORTANT_INDEX_DIR
+import time
+
 
 CORPUS_SIZE = 44845
+SECONDARY_INDEX_BODY = list()
+SECONDARY_INDEX_IMPORTANT = list()
+DOC_MAP = dict()
 
 def search(query_text, top_k=5):
     tokenized_query = tokenize(query_text)
     stemmer = PorterStemmer()
     stemmed_query = [stemmer.stem(token) for token in tokenized_query]
-    sorted_postings, all_terms_found = get_postings(stemmed_query, partial_indexes=False)
+    sorted_postings, all_terms_found = get_postings(stemmed_query)
     if not all_terms_found:
         return []
-    intersected_postings, idf_dict = boolean_AND_search(sorted_postings)
-    sorted_doc_scores = sorted(score_query(intersected_postings, idf_dict).items(), key=lambda x: x[1], reverse=True)
-    doc_map = None
-    with open("doc_id_map.json", "r") as file:
-        doc_map = json.load(file)
-    return [(doc_map[str(doc_id)], score) for doc_id, score in sorted_doc_scores[:top_k]]
+    intersected_postings = boolean_AND_search(sorted_postings)
+    sorted_doc_scores = sorted(score_query(intersected_postings).items(), key=lambda x: x[1], reverse=True)
+    return [(DOC_MAP[str(doc_id)], score) for doc_id, score in sorted_doc_scores[:top_k]]
 
 def search_query():
+    # External data structure setup
+    global SECONDARY_INDEX_BODY
+    global SECONDARY_INDEX_IMPORTANT
+    global DOC_MAP
+    SECONDARY_INDEX_BODY = load_secondary_index(SECONDARY_BODY_INDEX_PATH)
+    SECONDARY_INDEX_IMPORTANT = load_secondary_index(SECONDARY_IMPORTANT_INDEX_PATH)
+    with open("doc_id_map.json", "r") as file:
+        DOC_MAP = json.load(file)
+
     query = input("Enter your search query: ")
     results = search(query)
     if not results:
@@ -30,26 +44,14 @@ def search_query():
         print(f"Document: {url}, Score: {score}")
 
 # Get a list of all the postings for each term and remove duplicates and sort them by length
-# partial_indexes = true if we are using partial indexes
-def get_postings(stemmed_query: list[str], partial_indexes: bool) -> tuple[list[tuple[str, list[tuple[int, int]]]], bool]:
+def get_postings(stemmed_query: list[str]) -> tuple[list[tuple[str, list[tuple[int, int]]]], bool]:
     postings = []
     stemmed_set = set(stemmed_query)
     all_terms_found = True
-    inverted_index = None
-    if not partial_indexes:
-        with open("full_index_sorted.json", "r") as file:
-            inverted_index = json.load(file)
 
     for term in stemmed_set:
-        # ----------------Implement this later for full project and replace the term_postings---------------
-        # if partial_indexes:
-        #    
-        #     term_postings = get_postings_from_partial_indexes(term)
-        # else:
-        #     term_postings = get_postings_from_full_index(term, inverted_index)
-        # --------------------------------------------------------------------------------------------------
-
-        term_postings = get_postings_from_full_index(term, inverted_index)
+        # term_postings = get_postings_from_full_index(term, inverted_index)
+        term_postings = search_partial_index_for_term(SECONDARY_INDEX_BODY, BODY_INDEX_DIR, term)
         if not term_postings:
             all_terms_found = False
         postings.append((term, term_postings))
@@ -57,23 +59,12 @@ def get_postings(stemmed_query: list[str], partial_indexes: bool) -> tuple[list[
     sorted_postings = sorted(postings, key=lambda x: len(x[1]))
     return sorted_postings, all_terms_found
 
-# Takes in a term and returns the posting from the full index
-def get_postings_from_full_index(term: str, inverted_index) -> list[tuple[int, int]]:
-    if term in inverted_index:
-        return inverted_index[term]
-    return []
-
-# Performs a boolean AND search on the sorted postings lists and returns the dict of term: list of (doc_id, frequency) tuples and idf weights
-# Input: [(term, [(doct_id, frequency), ...]), ...]
-# Output: {term: [(doc_id, frequency), ...]}, {term: idf weight}
-def boolean_AND_search(sorted_postings: list[tuple[str, list[tuple[int, int]]]]) -> tuple[list[tuple[str, list[tuple[int, int]]]], dict[str, float]]:
+# Performs a boolean AND search on the sorted postings lists and returns the dict of term: list of (doc_id, tf-idf)
+# Input: [(term, [(doct_id, tf-idf), ...]), ...]
+# Output: {term: [(doc_id, tf-idf), ...]}
+def boolean_AND_search(sorted_postings: list[tuple[str, list[tuple[int, int]]]]) -> list[tuple[str, list[tuple[int, int]]]]:
     if not sorted_postings:
-        return [], {}
-
-    # Calculate IDF weights
-    idf_dict = dict()
-    for term, postings in sorted_postings:
-        idf_dict[term] = math.log(CORPUS_SIZE / len(postings))
+        return []
 
     # Get intersect of all documents in postings
     documents = intersect_documents(sorted_postings)
@@ -81,7 +72,7 @@ def boolean_AND_search(sorted_postings: list[tuple[str, list[tuple[int, int]]]])
     # Create new list with only intersected
     intersect_postings = filter_postings(sorted_postings, documents)
 
-    return intersect_postings, idf_dict
+    return intersect_postings
 
 def intersect_documents(indexes: list[tuple[str, list[tuple[int, int]]]]) -> set[int]:
     if not indexes:
@@ -104,26 +95,64 @@ def filter_postings(sorted_postings: list[tuple[str, list[tuple[int, int]]]], va
     return filtered_postings
     
 
-def score_query(sorted_postings, idf_dict):
+def score_query(sorted_postings):
     doc_scores = defaultdict(float)
 
     for term, posting_list in sorted_postings:
-        if term not in idf_dict:
-            continue
-        
-        idf_t = idf_dict[term]
-
-        for doc_id, tf_td in posting_list:
-            # tf weight: (1 + log(tf))
-            tf_weight = 1 + math.log(tf_td)
-
-            # w_{t,d} = (1 + log(tf)) * idf_t
-            w_t_d = tf_weight * idf_t
-
+        for doc_id, tf_idf_score in posting_list:
             # accumulate doc score
-            doc_scores[doc_id] += w_t_d
+            doc_scores[doc_id] += tf_idf_score
 
     return dict(doc_scores)
+
+def search_partial_index_for_term(secondary_index: list[tuple[str, str, str]], split_index_dir: str, term: str) -> list[tuple[int, int]] | None:
+    file = binary_search_secondary_index(secondary_index, term)
+    if not file:
+        return None
+    file_path = os.path.join(split_index_dir, file)
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+    return binary_search_partial_index(lines, term)
+
+def binary_search_secondary_index(secondary_index: list[tuple[str, str, str]], term: str) -> str | None:
+    low = 0
+    high = len(secondary_index) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        first_term, last_term, filename = secondary_index[mid]
+
+        if first_term <= term <= last_term:
+            return filename
+        elif term < first_term:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return None
+
+def binary_search_partial_index(lines: list[str], term: str) -> list[tuple[int, int]] | None:
+    low = 0
+    high = len(lines) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        mid_term_data = json.loads(lines[mid])
+        mid_term = list(mid_term_data.keys())[0]
+
+        if mid_term == term:
+            return mid_term_data[mid_term]
+        elif term < mid_term:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return None
+
+def load_secondary_index(path: str) -> list[tuple[str, str, str]]:
+    with open(path, 'rb') as f:
+        secondary_index = pickle.load(f)
+    return secondary_index
 
 if __name__ == '__main__':
     search_query()
